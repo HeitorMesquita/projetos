c5cea2f627a7e25d8aff2becc2e2b04294bf4524
HeitorMesquita <94998317+HeitorMesquita@users.noreply.github.com>
2021-12-03 18:54:36 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,3 @@
-
 var box;
 
 function setup() {
@@ -8,29 +7,34 @@ function setup() {
 
 function draw() 
 {
-  background(30);
-  if (keyDown("DOWN_ARROW")){
-    box.velocityX = 0;
-    box.velocityY = 3;
   
+  if (keyDown("DOWN_ARROW")){
+    background("green")
+    box.velocityX = 0; 
+    box.velocityY = 3;
+    
   }
   if (keyDown("UP_ARROW")){
+    background("blue")
     box.velocityX = 0;
     box.velocityY = -3;
-  
+    
   }
   if (keyDown("LEFT_ARROW")){
+    background("pink")
     box.velocityX = -3;
     box.velocityY = 0;
-  
+   
   }
   if (keyDown("RIGHT_ARROW")){
+    background("yellow")
     box.velocityX = 3;
     box.velocityY = 0;
-  
+
   }
     drawSprites();
 }
+
 
 
 
